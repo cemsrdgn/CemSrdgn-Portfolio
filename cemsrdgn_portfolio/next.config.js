a234caa2ff7817467ed6cfb2001e6275ff8b0197
Cem_Sarıdoğan <cemsrdgn@gmail.com>
2024-06-19 17:22:52 +0300

    fourth update for GitHub pages.

--- cemsrdgn_portfolio/next.config.js
+++ cemsrdgn_portfolio/next.config.js
@@ -5,3 +5,9 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+module.exports = {
+    images: {
+        // Disable image optimization during export
+        unoptimized: true,
+    },
+};
