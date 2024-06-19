0eea582f5f020f70deed0eb36ba431605e8d53b8
Cem_Sarıdoğan <cemsrdgn@gmail.com>
2024-06-19 20:41:08 +0300

    Updates

---
+++ _next/static/7e9WZEsBQzFY9g2pX6t_d/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/806-d49571afc4814cac.js","static/css/f2da0a177572737c.css","static/chunks/pages/index-ee1297341d166242.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
