ddfcd72d9176185e7c921171c069c518f15a78fb
Cem_Sarıdoğan <cemsrdgn@gmail.com>
2024-06-19 17:14:17 +0300

    third update for GitHub pages.

--- cemsrdgn_portfolio/next.config.mjs
+++
@@ -1,7 +0,0 @@
-const { PHASE_PRODUCTION_BUILD } = require('next/constants');
-
-module.exports = {
-    reactStrictMode: true,
-    basePath: '',
-    assetPrefix: process.env.NODE_ENV === PHASE_PRODUCTION_BUILD ? '/cemsrdgn.github.io/' : '',
-};
