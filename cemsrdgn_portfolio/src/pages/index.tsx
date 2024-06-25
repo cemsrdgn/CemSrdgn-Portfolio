3f55810a53d559486f29ab4fb3adf52e2f0377cb
Cem_Sarıdoğan <cemsrdgn@gmail.com>
2024-06-25 16:12:16 +0300

    tenth update for GitHub pages.

--- cemsrdgn_portfolio/src/pages/index.tsx
+++ cemsrdgn_portfolio/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Navbar from '../components/Navbar';
-import Home from '../components/Home';
+import Navbar from '@/components/Navbar';
+import Home from '@/components/Home';
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
